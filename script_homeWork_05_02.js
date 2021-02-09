cf62bc94290c8ffcfdff6dd2fe647a7a57a5bb41
mayson13 <motrik_xxl@mail.ru>
2021-02-09 17:36:54 +0300

    homework

---
+++ script_homeWork_05_02.js
@@ -0,0 +1,75 @@
+let name = 'John',
+admin = name;
+alert(admin);
+
+
+let planetEarth = 'Планета Земля',
+userName = 'Vasa';
+console.log(userName, planetEarth);
+
+
+let a = 10,
+b = 2;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+
+let str = 'Привет, Мир!';
+alert(str);
+
+
+let min = 37;
+if (min >= 1 && min<=15) {
+   alert('В первую четверть')
+} else if (min >= 16 && min <= 30) {
+   alert('Во второй четверти')
+} else if(min >=31 && min <= 45) {
+   alert('В третью четверть')
+} else if(min >= 46 && min <= 59) {
+   alert('В четвертую четверть')
+}
+
+
+// let min = 37;
+// switch(min) {
+//    case min >= 1 && min <= 15:
+//       alert('В первую четверть');
+//       break;
+//    case min >= 16 && min <= 30:
+//       alert('Во второй четверти');
+//       break;
+//    case min >= 31 && min <= 45:
+//       alert('В третью четверть');
+//       break;
+//    case min >= 46 && min <= 59:
+//       alert('В четвертую четверть');
+//       break;
+// }
+
+
+// через if
+let lang = 'en';
+if (lang == 'en') {
+   country = 'Eng';
+} else if (lang == 'ru') {
+   country = 'Раша';
+}
+alert(country);
+
+
+//через switch
+// let lang = 'en';
+// switch(lang) {
+// case 'ru':
+//    country = 'Раша';
+//    break;
+// case  'en':
+//    country = 'Eng';
+//    break;
+// }
+// alert(country);
+
+
